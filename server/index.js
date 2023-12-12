c9d3ba3c277319963a083a4bcc3d42002a4e7293
mortale2004 <pawanmortale111@gmail.com>
2023-12-12 11:20:38 +0530

    updated headers for cors

--- server/index.js
+++ server/index.js
@@ -26,6 +26,23 @@ app.use("/api/notes/", require("./routes/noteRouter"));
 app.use("/api/", require("./routes/messageRouter"));
 
 
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
+  
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res)=>{
