271ae10f957d57e6be2748c723a79523fc56dc1b
mortale2004 <114370142+mortale2004@users.noreply.github.com>
2023-12-12 11:59:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const cors = require("cors");
 const path = require("path");
 
 
-app.use(cors());
+app.use(cors({
+    origin: "https://inotes-ckzr.onrender.com/"
+}));
 
 // port no
 const PORT = process.env.PORT;
@@ -25,23 +27,6 @@ app.use("/api/notes/", require("./routes/noteRouter"));
 
 app.use("/api/", require("./routes/messageRouter"));
 
-
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
-  })
   
 app.use(express.static(path.join(__dirname, "../client/build")));
 
